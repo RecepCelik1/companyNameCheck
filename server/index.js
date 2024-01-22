23d98dd095fd893ac441fdd8c434b1436dac5b73
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2024-01-22 17:56:05 +0300

    .

--- server/index.js
+++ server/index.js
@@ -16,28 +16,45 @@ const apiKey = '8dc91acf-5646-4c5d-abef-5524ea035071';
 const PORT = 5000;
 
 app.get('/company/:companyName', async (req, res) => {
-    const companyName = req.params.companyName;
-    const apiUrl = `https://api.company-information.service.gov.uk/advanced-search/companies?company_name_includes=${companyName}`;
+  const startRequestTime = Date.now(); 
+
+  const companyName = req.params.companyName;
+
   
-    try {
+  const apiUrl = `https://api.company-information.service.gov.uk/advanced-search/companies?company_name_includes=${companyName}&size=5000&company_status=active`;
+  try {
       const response = await fetch(apiUrl, {
-        headers: {
-          'Authorization': `Basic ${btoa(apiKey + ':')}`
-        }
+          headers: {
+              'Authorization': `Basic ${btoa(apiKey + ':')}`
+          }
       });
-  
+
       if (!response.ok) {
-        throw new Error('API request failed');
+          throw new Error('API request failed');
       }
-  
+
       const data = await response.json();
-      res.json(data);
-    } catch (error) {
+
+      const itemsArray = data.items || [];
+
+      const companyNameArray = itemsArray.map(item => item.company_name);
+
+      const endRequestTime = Date.now();
+      const requestDuration = endRequestTime - startRequestTime;
+
+      console.log(`API request took ${requestDuration} milliseconds`);
+
+      for(companyNameArray ; 0<=companyNameArray ; companyNameArray--){
+        
+      }
+
+      res.json(companyNameArray);
+  } catch (error) {
       res.status(500).json({ error: error.message });
-    }
-  });
+  }
 
-
+  
+});
 
 
 app.listen(PORT , () => {
